d23fb83eb109fcf97e64b46eeb4446323479c0e7
Etienne <bissonnette@live.ca>
2023-05-26 15:32:51 -0400

    Login Form completed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import { GoSignIn } from "react-icons/go";
 
 function Login() {
-  return <h1>Login</h1>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password, confirmPassword } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <GoSignIn style={{ marginRight: "10px" }} />
+          Login
+        </h1>
+        <p>Sign in and start setting goals</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Log In
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default Login;
